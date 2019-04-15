e5ad92f2091f565d7ed3b90f52de371b8766f360
Hunter <huntercavers@xtra.co.nz>
2019-04-15 22:46:46 +1200

    First commit of Node class

---
+++ Node.java
@@ -0,0 +1,57 @@
+// Hunter Cavers (1288108)
+// Sivarim Manoharan (1299026)
+
+// Import libraries needed
+import java.util.List;
+import java.util.ArrayList;
+
+// Nodes will be used in dictionary
+class Node
+{
+	// The index of the node in the dictionary
+	private int _index;
+	// The value assigned to the list
+	private byte _value;
+	// A collection of Nodes that will act a children in the multi way trie
+	private List<Node> _children;
+	
+	// Constructor to create new nodes
+	public Node(int index, byte value)
+	{
+		_index = index;
+		_value = value;
+		_children = new ArrayList<Node>();
+	}
+	
+	// Allows read only access to value attribute
+	public byte getValue()
+	{
+		return _value;
+	}
+	
+	// Allows read only access to index attribute
+	public int getIndex()
+	{
+		return _index;
+	}
+	
+	// Creates a node and adds it to list of children
+	public void addChild(int index, byte value)
+	{
+		Node node = new Node(index, value);
+		_children.add(node);
+	}
+	
+	// Returns the index of the child that contains the input or returns 0
+	public int childrenContain(byte value)
+	{
+		for(Node node : _children)
+		{
+			if(node.getValue() == value)
+			{
+				return node.getIndex();
+			}
+		}
+		return 0;
+	}
+}
