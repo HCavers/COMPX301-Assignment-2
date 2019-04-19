0bdb5f13f82382eb1cb36f9eecc8dbd02700cefb
Sivaram Manoharan <sivaramigcse@gmail.com>
2019-04-20 01:43:56 +1200

    Create LZpack.java
    Added base code for LZpack, still testing out the best way to read the byte stream however.

---
+++ LZpack.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+public class LZpack {
+
+public static void main(String[] args) {	
+	double pattNum = Double.valueOf(args[0]);
+	InputStream bitStream = new BufferedInputStream(System.in);
+	byte [] byteArray = new byte[1024];
+	try {
+		bitStream.read(byteArray);
+		//String s = new String(byteArray);
+		//System.out.println("Contents of the byte stream are :: "+ s);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+}
+
+public String encodeLine(String Line, int minBits) {
+	return null;
+}
+public void shiftLeft(int value, int shift) {
+	value = value << shift;
+}
+
+public void shiftRight(int value, int shift) {
+	value = value >> shift;
+}
+
+public void uShiftRight(int value, int shift) {
+	value = value >>> shift;
+}
+	
+public void mask(int value, int mask) {
+	value = value & mask;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
