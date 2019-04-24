5c5089f74e51df2387f1257a26182281cd62af28
Sivaram Manoharan <sivaramigcse@gmail.com>
2019-04-25 00:43:07 +1200

    Updated LZpack
    Few more changes needed to make it work, should be done tommorow.

--- LZpack.java
+++ LZpack.java
@@ -1,27 +1,49 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class LZpack {
 
 public static void main(String[] args) {	
-	double pattNum = Double.valueOf(args[0]);
-	InputStream bitStream = new BufferedInputStream(System.in);
-	byte [] byteArray = new byte[1024];
+//	double pattNum = Double.valueOf(args[0]);
+//	InputStream bitStream = new BufferedInputStream(System.in);
+//	byte [] byteArray = new byte[1024];
+//	try {
+//		bitStream.read(byteArray);
+//		//String s = new String(byteArray);
+//		//System.out.println("Contents of the byte stream are :: "+ s);
+//	} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+	File input = new File("input.txt");
 	try {
-		bitStream.read(byteArray);
-		//String s = new String(byteArray);
-		//System.out.println("Contents of the byte stream are :: "+ s);
-	} catch (IOException e) {
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		String Line = br.readLine();
+		
+		while(Line != null) {
+			encodeLine(Line,5);
+			Line = br.readLine();
+		}
+	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	}
+	} 
 	
 	
 }
 
-public String encodeLine(String Line, int minBits) {
+public static String encodeLine(String Line, int minBits) {
+	String[] lineValues = Line.split(",");
+	int index = Integer.parseInt(lineValues[0]);
+	char character = lineValues[1].charAt(0);
+	System.out.println("Index: " + index);
+	System.out.println("Character: " + character);
 	return null;
 }
 public void shiftLeft(int value, int shift) {
@@ -42,6 +64,14 @@ public void mask(int value, int mask) {
 
 public void copy(int value, int copy) {
 	value = value | copy;
+}
+
+public int calculateOffset(int value) {
+	   int offset = Integer.SIZE-Integer.numberOfLeadingZeros(value);
+	   if(offset == 0) {
+		   return 1;
+	   }
+	   return offset;
 }
 	
 public void pack(int value, int mask, int phraseNum, int charMismatched, int pOffset, int cOffset) {
