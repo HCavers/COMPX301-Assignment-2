49018f01d83fb43c9659a13c39384f02c0894585
maravis <sivaramigcse@gmail.com>
2019-04-27 17:11:52 +1200

    Fixed output

--- LZunpack.java
+++ LZunpack.java
@@ -11,31 +11,10 @@ public class LZunpack {
 	public static void main(String[] args) {	
 		int counter = 1;
 		int output = 0;
-		//int output1 = 0b10100000110100001010010000110000;
-	
-		//int output2 = 0b1101000001;
-//		File input = new File("output.txt");
-//		try {
-//			BufferedReader br = new BufferedReader(new FileReader(input));
-//			String Line = br.readLine();
-//			int output1 = 0b1010000011010000101001000011;
-//			int output2 = 0b1101000001;
-//			while(Line != null) {
-//				//output = Integer.parseInt(Line);
-//				output = readData(output1,counter);
-//				counter++;
-//				Line = br.readLine();
-//				
-//			}
-//			br.close();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		//System.in.read();
 		try {
-			while(System.in.available() != 0) {
-				output = getNextPair(output,counter);
+			int line = System.in.read();
+			while(line != 0) {
+				output = getNextPair(output,counter,line);
 				counter++;
 			}
 			
@@ -61,11 +40,11 @@ public static int generateMask(int length, int maxBits) {
 	return mask;
 }
 
-public static int getNextPair(int value, int counter) {
+public static int getNextPair(int value, int counter, int input) {
 	int bitsNeeded = bitsNeeded(counter) + 8;
 	
 	try {
-		int line = System.in.read();
+		int line = input;
 		while (buffAmount < bitsNeeded) {
 			if (line != 0) {
 				value = readByte(line,value);
@@ -95,20 +74,13 @@ public static int readByte(int data,int value) {
 
 
 public static int readData(int line,int counter) {
-	//System.out.println("VALUE BEFORE SHIFT = " + Integer.toBinaryString(line));
 	int iBits = bitsNeeded(counter);
-	//System.out.println("READING " + iBits + " BITS");
 	int mask = generateMask(iBits,32);
-	//System.out.println(Integer.toBinaryString(mask));
 	int phrase = (line & mask) >>> (32 - iBits);
-	//System.out.println(Integer.toBinaryString(phrase));
 	line = line << iBits;
-	//System.out.println("VALUE AFTER SHIFT  = " + Integer.toBinaryString(line));
 	int cBits = 8;
-	//System.out.println("READING " + cBits + " BITS");
 	mask = generateMask(8,32);
 	int character = (line & mask)  >>> (24);
-	//System.out.println(Integer.toBinaryString(character));
 	char c = (char) character;
 	line = line << 8;
 	System.out.println(phrase + "," + character );
