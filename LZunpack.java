cb785154f4dbb34504da791afb43b769936217b9
Sivaram Manoharan <sivaramigcse@gmail.com>
2019-04-27 20:52:44 +1200

    Tidied code a bit

--- LZunpack.java
+++ LZunpack.java
@@ -1,14 +1,15 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 // Hunter Cavers (1288108)
 // Sivaram Manoharan (1299026)
+
+import java.io.IOException;
+
 public class LZunpack {
+	
+	public static int cBits = 8;
 	public static int buffAmount = 0;
 	public static int line;
+	public static int maxBits = 32;
+	
 	public static void main(String[] args) {	
 		int counter = 1;
 		int output = 0;
@@ -27,65 +28,54 @@ public class LZunpack {
 		
 	}
 
-
 	public static int bitsNeeded(int counter) {
 		   double bits = Math.log(counter)/Math.log(2.0);
 		   return ((int)(bits)) + 1;
 	}
 
-public static int generateMask(int length, int maxBits) {
-	int offset = maxBits - length;
-	int mask = 1 << length;
-	mask--;
-	mask <<= offset;
-	return mask;
-}
+	public static int generateMask(int length, int totalBits) {
+		int offset = totalBits - length;
+		int mask = 1 << length;
+		mask--;
+		mask <<= offset;
+		return mask;
+	}	
 
-public static int getNextPair(int value, int counter) {
-	int bitsNeeded = bitsNeeded(counter) + 8;
-	
-	try {
-		
-		while (buffAmount <= bitsNeeded) {
-			if (line != -1) {
-				value = readByte(line,value);
-				line = System.in.read();
+	public static int getNextPair(int value, int counter) {
+		int iBits = bitsNeeded(counter);
+		int bitsNeeded = iBits + cBits;
+		try {
+			while (buffAmount <= bitsNeeded) {
+				if (line != -1) {
+					value = readByte(line,value);
+					line = System.in.read();
+				}else break;
 			}
-			else break;
-			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+		value = readData(value,iBits);
+		buffAmount = buffAmount - bitsNeeded;
+		return value;
 	}
-	
-	value = readData(value,counter);
-	buffAmount = buffAmount - bitsNeeded;
-	return value;
-	
-}
 
-public static int readByte(int data,int value) {
-	int shift = 24 - buffAmount;
-	data = data << shift;
-	buffAmount = buffAmount + 8;
-	value = value | data;
-	return value;
-}
+	public static int readByte(int data,int value) {
+		int shift = (maxBits - cBits) - buffAmount;
+		data = data << shift;
+		buffAmount = buffAmount + cBits;
+		value = value | data;
+		return value;
+	}
 
-
-public static int readData(int value,int counter) {
-	int iBits = bitsNeeded(counter);
-	int mask = generateMask(iBits,32);
-	int phrase = (value & mask) >>> (32 - iBits);
-	value = value << iBits;
-	int cBits = 8;
-	mask = generateMask(8,32);
-	int character = (value & mask)  >>> (24);
-	char c = (char) character;
-	value = value << 8;
-	System.out.println(phrase + "," + character );
-	return value;
-}
-
+	public static int readData(int value,int iBits) {
+		int mask = generateMask(iBits,maxBits);
+		int phrase = (value & mask) >>> (maxBits - iBits);
+		value <<= iBits;
+		mask = generateMask(cBits,maxBits);
+		int character = (value & mask)  >>> (maxBits - cBits);
+		value <<= cBits;
+		System.out.println(phrase + "," + character );
+		return value;
+	}
 }
