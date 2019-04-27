a395db4835eeaf21a7641c06140a3a564c17c198
Sivaram Manoharan <sivaramigcse@gmail.com>
2019-04-27 22:06:26 +1200

    Finished Commenting LZpack

--- LZpack.java
+++ LZpack.java
@@ -23,7 +23,7 @@ public class LZpack {
 				counter++; // Increments the counter
 			}
 			br.close(); // Closes the buffer
-			int numOut = currUsedBits / cBits; // Calculates the number of bytes that can be outputted from the buffer
+			int numOut = currUsedBits / cBits; // Calculates the number of bytes that can be output from the buffer
 			for(int i = 0; i< numOut; i ++) { 
 				outputBytes(value); // Outputs a byte
 				value <<= cBits;	// Shifts the buffer by one byte length		
@@ -32,68 +32,67 @@ public class LZpack {
 				outputBytes(value); // Outputs the remaining bits in the case that the bits in the buffer were not a multiple of 8
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
 
-	public static int bitsNeeded(int counter) {
+	public static int bitsNeeded(int counter) { // Takes in the number of phrases and returns the number of bits needed to encode
 		double bits = Math.log(counter)/Math.log(2.0);
-		return ((int)(bits)) + 1;
+		return ((int)(bits)) + 1; // Returns log2(counter) rounded down and then incremented
 	}
 
-	public static int generateMask(int length, int totalBits) {
-		int offset = totalBits - length;
-		int mask = 1 << length;
-		mask--;
-		mask <<= offset;
+	public static int generateMask(int length, int totalBits) { // Creates an integer with the first X bits set where X is the length
+		int offset = totalBits - length; // Calculates the difference between the length and total bits
+		int mask = 1 << length; // Shifts 1 by the length
+		mask--; // Subtracts 1 from the mask to set all the bits preceding the 1
+		mask <<= offset; // Shifts the 1's up to the front
 		return mask;
 	}
 
-	public static int encodeLine(String Line, int counter, int value) {
-		String[] lineValues = Line.split(",");
-		int index = Integer.parseInt(lineValues[0]);
-		int character = Integer.parseInt(lineValues[1]);
-		int iBits = bitsNeeded(counter);
-		int availableBits = maxBits - currUsedBits;
-		if(iBits > availableBits) {
-			value = makeSpace(value);
+	public static int encodeLine(String Line, int counter, int value) { // Encodes a line and stores it in the buffer
+		String[] lineValues = Line.split(","); // Splits the line into its phrase and mismatched byte
+		int index = Integer.parseInt(lineValues[0]); // Stores the phrase in the Integer index
+		int character = Integer.parseInt(lineValues[1]); // Stores the mismatched byte in the Integer character
+		int iBits = bitsNeeded(counter); // Calculates the number of bits to encode the phrase
+		int availableBits = maxBits - currUsedBits; // Calculates the available bits by subtracting the used bits from the max bits
+		if(iBits > availableBits) { // Checks if the bits needed to code the phrase is greater than the available bits
+			value = makeSpace(value); // If so create space by outputting two bytes 
+			availableBits += doubleByte; // Increase the available bits by the two bytes of space
+		}
+		availableBits -= iBits; // Reduce available bits by the bits needed to code the phrase
+		value = pack(value,index,availableBits); // Pack the phrase into the buffer
+		currUsedBits += iBits; // Increase the currently used bits by the bits needed to code the phrase
+		if(cBits > availableBits) { // Checks if the bits needed to code the character is greater than the available bits
+			value = makeSpace(value); 
 			availableBits += doubleByte;
 		}
-		availableBits -= iBits;
-		value = pack(value,index,availableBits);
-		currUsedBits += iBits;
-		if(cBits > availableBits) {
-			value = makeSpace(value);
-			availableBits += doubleByte;
-		}
-		availableBits -= cBits;
+		availableBits -= cBits; 
 		value = pack(value,character,availableBits);
-		currUsedBits += cBits;
+		currUsedBits += cBits; // Packs the character into the buffer using the same process used to pack the phrase and modify the relevant variables
 		return value;
 	}
 	
 	public static int pack(int value, int inputBits, int bitOffset) {
-		int mask = generateMask(currUsedBits,maxBits);
-		value = value & mask;
-		inputBits <<= bitOffset;
-		value = value | inputBits;
+		int mask = generateMask(currUsedBits,maxBits); // Generates the mask for the current amount of bits
+		value = value & mask; // Clears any unused bits
+		inputBits <<= bitOffset; // Shifts the inputBits into the correct position
+		value = value | inputBits; // Copies the input bits into the buffer
 		return value;
 	}
 	
 	public static int makeSpace(int value) {
-		byte firstOut = (byte) ((value & generateMask(cBits,maxBits))>>> (maxBits - cBits)) ;
-		byte secondOut = (byte) ((value &  0x00FF0000)>>> doubleByte);
+		byte firstOut = (byte) ((value & generateMask(cBits,maxBits))>>> (maxBits - cBits)) ; // Store the first 8 bits of the buffer in a byte 
+		byte secondOut = (byte) ((value &  0x00FF0000)>>> doubleByte); // Store the second 8 bits of the buffer in a byte
 		System.out.write(firstOut);
-		System.out.write(secondOut);
-		System.out.flush();
-		currUsedBits = currUsedBits - doubleByte;
-		value <<= doubleByte;
+		System.out.write(secondOut); // Write both bytes out to standard out
+		System.out.flush(); 
+		currUsedBits = currUsedBits - doubleByte; // Reduces the currently used bits by the size of the two bytes
+		value <<= doubleByte; // Shifts the remaining 16 bits into the left position
 		return value;
 	}
 
-	public static void outputBytes(int value) {
-		byte output = (byte) ((value & generateMask(cBits,maxBits))>>> (maxBits - cBits)) ;
+	public static void outputBytes(int value) { // Outputs the first 8 bits in the buffer to standard out
+		byte output = (byte) ((value & generateMask(cBits,maxBits))>>> (maxBits - cBits)) ; 
 		System.out.write(output);
 		System.out.flush();
 	}
